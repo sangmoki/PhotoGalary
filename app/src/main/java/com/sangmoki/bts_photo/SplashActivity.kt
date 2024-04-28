31d5237e280a8b304fb14ee54458cc113050be3d
이상목 <yoochang4176@naver.com>
2024-04-28 13:56:31 +0900

    Feat: "앱 시작 시 Splash 추가"

---
+++ app/src/main/java/com/sangmoki/bts_photo/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.sangmoki.bts_photo
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed(Runnable {
+            // 앱의 MainActivity로 넘어가기
+            val i = Intent(this@SplashActivity,MainActivity::class.java)
+            startActivity(i)
+            // 현재 액티비티 닫기
+            finish()
+        }, 1000) // 3초
+    }
+}
